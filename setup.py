46e2562de648a9543ee0c29ce214c743d4032e3a
John Crawford <jrcrawfo@ncsu.edu>
2014-10-01 20:50:01 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ desc = 'A Python library for SIS resources to use for various common tasks'
 
 setup(
   name='sis-utils',
-  version='0.2.32',
+  version='0.2.4',
   description=desc,
   long_description=open(README).read(),
   package_dir={'sisutils': 'sisutils','sisutils.config':'sisutils/config','sisutils.mq':'sisutils/mq','sisutils.endpoint':'sisutils/endpoint','sisutils.email':'sisutils/email'},
